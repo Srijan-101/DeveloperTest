6621b97d7b330b147578bc34e9d90e2a703adc82
Srijankc <kcsrijan33@gmail.com>
2022-09-18 09:57:47 +0545

    make it better

--- build/src/App.js
+++ build/src/App.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
-const config = require('config');
+
 const cors = require('cors');
 const path = require("path");
 const app_routes_1 = __importDefault(require("./Routes/app.routes"));
-const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 app.use(cors());
